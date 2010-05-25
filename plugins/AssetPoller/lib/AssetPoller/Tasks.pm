214c8228ac182bac4c401d97b7cc5972e6c2212d
David Raynes <rayners@gmail.com>
2010-05-24 23:34:44 -0400

    Ran through perltidy

--- plugins/AssetPoller/lib/AssetPoller/Tasks.pm
+++ plugins/AssetPoller/lib/AssetPoller/Tasks.pm
@@ -54,21 +54,25 @@ PATH:
             next FILE if ( $f =~ /^\./ );
             my $file = File::Spec->catfile( $path, $f );
             next FILE unless ( -f $file );
-            
+
             # Grab the file filters, so that the polled directory contents can
             # be properly filtered to find only the desired files.
-            my $file_filter = $p->get_config_value('file_filter', 'blog:'.$blog_id);
+            my $file_filter
+                = $p->get_config_value( 'file_filter', 'blog:' . $blog_id );
             if ($file_filter) {
-                # Remove any space before or after the comma separator, and build an array.
-                my @file_filters = split(/\s*,\s*/, $file_filter);
+
+	      # Remove any space before or after the comma separator, 
+	      # and build an array.
+                my @file_filters = split( /\s*,\s*/, $file_filter );
                 my $skip;
                 foreach my $filter (@file_filters) {
-                    # Use the $skip "flag" to indicate whether a filter matched.
-                    # If the filter matches the filename, mark it to be skipped.
-                    # (This gives the chance for all filters to run before
-                    # deciding if it should be skipped.) If it's not a match,
-                    # pass the previously-set $skip so that the previous state
-                    # is still saved.
+
+                  # Use the $skip "flag" to indicate whether a filter matched.
+                  # If the filter matches the filename, mark it to be skipped.
+                  # (This gives the chance for all filters to run before
+                  # deciding if it should be skipped.) If it's not a match,
+                  # pass the previously-set $skip so that the previous state
+                  # is still saved.
                     $skip = ( $f =~ /$filter/i ) ? 1 : $skip;
                 }
                 next unless $skip;
@@ -78,40 +82,51 @@ PATH:
             my $asset_pkg;
 
             # Does the user want to import files that have been updated?
-            my $import_updated = $p->get_config_value('import_updated_files', 'blog:'.$blog_id);
+            my $import_updated = $p->get_config_value( 'import_updated_files',
+                'blog:' . $blog_id );
             if ($import_updated) {
+
                 # If any newer files are found, they should be imported.
                 # Grab the epoch time for the new file.
-                my $file_stamp = (stat($file))[9];
+                my $file_stamp = ( stat($file) )[9];
 
-                # Grab the existing asset, and its time.
-                # Use class "*" so that images, files, and any other class of object are returned.
-                $asset = MT->model('asset')->load({ blog_id => $blog_id, class => '*', file_name => $f });
+		# Grab the existing asset, and its time.
+		# Use class "*" so that images, files, and 
+		# any other class of object are returned.
+                $asset
+                    = MT->model('asset')
+                    ->load(
+                    { blog_id => $blog_id, class => '*', file_name => $f } );
                 if ($asset) {
-                    my $existing_stamp = ts2epoch($blog, $asset->modified_on);
+                    my $existing_stamp
+                        = ts2epoch( $blog, $asset->modified_on );
 
                     # Only proceed if the file is newer than the asset.
-                    next if ($file_stamp <= $existing_stamp);
+                    next if ( $file_stamp <= $existing_stamp );
 
-                    # The file is newer than the existing asset. So, just import it.
-                    $asset->modified_on( epoch2ts($blog, $file_stamp) );
+		    # The file is newer than the existing asset. 
+		    # So, just import it.
+                    $asset->modified_on( epoch2ts( $blog, $file_stamp ) );
                 }
                 else {
+
                     # The file hasn't been imported yet, so create a new asset
                     $asset_pkg = MT::Asset->handler_for_file($f);
-                    $asset = $asset_pkg->new();
+                    $asset     = $asset_pkg->new();
                 }
             }
             else {
-                # Don't import newer files, just ignore them.
-                # Use class "*" so that images, files, and any other class of object are returned.
+
+	      # Don't import newer files, just ignore them.
+	      # Use class "*" so that images, files, and 
+	      # any other class of object are returned.
                 next
                     if MT::Asset->exist(
-                            { blog_id => $blog->id, class => '*', file_name => $f } );
+                    { blog_id => $blog->id, class => '*', file_name => $f } );
 
                 # But we do want to import any new asset.
                 $asset_pkg = MT::Asset->handler_for_file($f);
-                $asset = $asset_pkg->new();
+                $asset     = $asset_pkg->new();
             }
 
             # turn the file into an asset
@@ -162,15 +177,15 @@ PATH:
             my $meth = ( $remove_files ? 'rename' : 'put' );
             $fmgr->$meth( $file, $dest_file ) or die $fmgr->errstr;
             $asset->save;
-            
-            MT->log({
-                level   => MT->model('log')->INFO(),
-                blog_id => $blog_id,
-                message => MT->translate(
-                    'Asset Poller has imported the file [_1].',
-                    $f,
-                ),
-            });
+
+            MT->log(
+                {   level   => MT->model('log')->INFO(),
+                    blog_id => $blog_id,
+                    message => MT->translate(
+                        'Asset Poller has imported the file [_1].', $f,
+                    ),
+                }
+            );
 
         }
 
